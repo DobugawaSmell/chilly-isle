812577d66db8aad5162e42d00fe6630cc4ef4f9d
DobugawaSmell <76649989+DobugawaSmell@users.noreply.github.com>
2021-11-24 22:43:39 +0900

    fabikonn

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Title from './components/title'
 import Top from './components/top.js'
 import Works from './components/work.js'
 import Footer from './components/footer.js'
+import Favicon from './img/favicon.ico'
 
 export default function Home({index})
 {
@@ -14,6 +15,7 @@ export default function Home({index})
     <>
       <Head>
         <title>chilly isle</title>
+        <link rel="icon" href={Favicon} id="favicon" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
